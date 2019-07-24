baa02dd81f48c860706d1f67f1838927965ff3c1
Florian Krämer <florian.kraemer@kreative-design.net>
2019-07-25 00:17:33 +0200

    Adding test case skeletons

---
+++ tests/TestCases/Application/Http/RequestHandler/Mailers/Listing.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+/**
+ * Copyright (c) Florian Krämer (https://florian-kraemer.net)
+ *
+ * Licensed under The GPL3 License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Florian Krämer (https://florian-kraemer.net)
+ * @author        Florian Krämer
+ * @link          https://github.com/Phauthentic
+ * @license       https://opensource.org/licenses/GPL-3.0 GPL3 License
+ */
+namespace App\Test\TestCase\Application\Http\RequestHandler\Mailer;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Listing Test
+ */
+class Listing extends TestCase
+{
+    /**
+     * @return void
+     */
+    public function testCreating(): void
+    {
+
+    }
+}
